079bc9695289b49064b079469c8c985c4adef4d4
Gleb Lekomtsev <lekomcevg@gmail.com>
2024-05-30 16:10:57 +0500

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+// Галерея и лайтбоксы от Fancybox
+import { Fancybox } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+
+
+// Мобильная навигация
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
