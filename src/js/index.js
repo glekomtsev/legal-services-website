fb41ded7774b33227ac40afe611ad1a4872a8a35
Gleb Lekomtsev <lekomcevg@gmail.com>
2024-05-31 16:43:38 +0500

    add slider services and reviews

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,23 @@ import Swiper from "swiper/bundle";
 // import styles bundle
 import "swiper/css/bundle";
 
-const swiper = new Swiper(".swiper", {
+const swiperServices = new Swiper(".swiper-services", {
   slidesPerView: "auto",
   spaceBetween: 24,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+});
+
+const swiperLawyers = new Swiper(".swiper-lawyer", {
+  slidesPerView: "auto",
+  spaceBetween: 24,
+});
+
+const swiperReviews = new Swiper(".swiper-reviews", {
+  slidesPerView: "auto",
+  spaceBetween: 24,
+
+  navigation: {
+    nextEl: ".reviews__right-spwipe",
+    prevEl: ".reviews__left-spwipe",
   },
 });
 
