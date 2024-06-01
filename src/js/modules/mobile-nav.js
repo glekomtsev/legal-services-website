9441f718cfe2fa441f2862bdd7ce1b9b8ee5c867
Gleb Lekomtsev <lekomcevg@gmail.com>
2024-06-01 21:44:07 +0500

    add bav mobile

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,21 @@
 function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+  // Mobile nav button
+  const navBtn = document.querySelector("#mobile-nav-btn");
+  const nav = document.querySelector(".mobile-nav");
+  const menuIcon = document.querySelector("#nav-icon");
+  const navClose = document.querySelector("#close-mobile-nav");
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+  navBtn.onclick = function() {
+    nav.classList.toggle("mobile-nav--open");
+    menuIcon.classList.toggle("nav-icon--active");
+    document.body.classList.toggle("no-scroll");
+  };
+
+  navClose.onclick = function() {
+    nav.classList.remove("mobile-nav--open");
+    menuIcon.classList.remove("nav-icon--active");
+    document.body.classList.toggle("no-scroll");
+  };
 }
 
 export default mobileNav;
